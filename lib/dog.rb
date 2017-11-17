6c872b7662fd51886eb9dbe100315ef9a5ab5eea
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2017-11-17 22:51:51 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+  def initialize(beagle)
+    @breed = beagle
+  end
+  def breed=(beagle)
+    @breed = beagle
+  end
+  def breed
+    @breed
+  end
+  def initialize(fido)
+    @name = fido
+  end
+  def name=(fido)
+ @name = fido
+ end
+ def name
+   @name
+ end
+end
+fido=Dog.new(beagle)
+fido.name = "fido"
+fido.breed = "beagle"
